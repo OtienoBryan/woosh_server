e40dd6c668d7cb292fd16365b2ead0e223402e1d
OtienoBryan <bo9511221@gmail.com>
2025-08-04 20:34:57 +0300

    envs

--- server.js
+++ server.js
@@ -1351,7 +1351,7 @@ app.use((err, req, res, next) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
+    origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [
       'https://woosh-client.vercel.app',
       'http://localhost:5173',
       'http://localhost:3000'

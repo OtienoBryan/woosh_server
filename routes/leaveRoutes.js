8de4ea5dbcda1662687e8b1ea1092571f150a706
OtienoBryan <bo9511221@gmail.com>
2025-08-18 17:49:08 +0300

    leaves

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const leaveController = require('../controllers/leaveController');
 
 router.get('/sales-rep-leaves', leaveController.getAllSalesRepLeaves);
+router.patch('/:id/status', leaveController.updateLeaveStatus);
 
 module.exports = router; 
